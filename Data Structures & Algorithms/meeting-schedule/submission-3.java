7acc4678d97907d91073dd70654193f6659a49c5
Harsh Saxena <Saxenaharsh50984@gmail.com>
2026-08-07 22:54:55 +0530

    Add: meeting-schedule - submission-3

---
+++ Data Structures & Algorithms/meeting-schedule/submission-3.java
@@ -0,0 +1,34 @@
+/**
+ * Definition of Interval:
+ * public class Interval {
+ *     public int start, end;
+ *     public Interval(int start, int end) {
+ *         this.start = start;
+ *         this.end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+    public boolean canAttendMeetings(List<Interval> intervals) {
+
+       
+
+        if(intervals == null || intervals.size() <= 1)return true;
+
+        Collections.sort(intervals,(a,b) -> Integer.compare(a.start,b.start));
+
+               
+        int n = intervals.size();
+        for(int i = 1; i < n; i++){
+          if(intervals.get(i).start < intervals.get(i-1).end){
+                return false;
+
+            }
+        }
+
+
+return true;
+
+    }
+}
